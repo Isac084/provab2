f010b2bddf5bc39c9aeff8e60312deae03a666a1
Isac084 <isac.nichollas09@mail.com>
2025-07-09 10:06:25 -0300

    sla

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const toggle = document.querySelector(".accordion-toggle");
+const content = document.querySelector(".accordion-content");
+
+toggle.addEventListener("click", () => {
+    content.classList.toggle("open");
+});
+
+
+
+
+
